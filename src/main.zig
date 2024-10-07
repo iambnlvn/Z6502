ab2789d3fe48a93940d60a41f82384cf4870b6f9
iambnlvn <iambnlvn@gmail.com>
2024-10-07 09:59:09 -0500

    feat: Implement memory dump functionality
    This commit adds the functionality to dump the virtual memory of the CPU to a file. The `dumpVirtualMemory` function is implemented, which writes the contents of the `sysMem` data to a file named "dump.bin". This feature allows for debugging and analysis of the memory state.

--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,35 @@
 const std = @import("std");
+const fs = std.fs;
+const cpu = @import("Cpu.zig").CPU;
 
-pub fn main() !void {}
+pub fn main() !void {
+    var arenaInstance = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arenaInstance.deinit();
+
+    var arena = arenaInstance.allocator();
+    const prog = @embedFile("print.bin");
+    var cpu6502 = try cpu.init(&arena);
+
+    defer cpu6502.bus.deinit();
+    @memcpy(cpu6502.bus.sysMem.data, prog);
+
+    // try dumpVirtualMemory(&cpu6502);
+    cpu6502.reset();
+
+    while (true) {
+        cpu6502.clock();
+    }
+}
+fn dumpVirtualMemory(proc: *cpu) !void {
+    var cwd = fs.cwd();
+
+    var f = try cwd.createFile("dump.bin", .{});
+    defer f.close();
+
+    try f.writeAll(proc.bus.sysMem.data);
+
+    // for printing memory
+    // for (0..0x10000) |i| {
+    //     std.debug.print("{x}", .{proc.bus.sysMem.data[i]});
+    // }
+}
